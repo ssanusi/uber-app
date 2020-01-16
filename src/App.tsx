e0c8b77ea67c06718a9f633fa7ae0269b75af497
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-01-16 16:46:12 +0100

    Hookup apollo to React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import { IS_LOGGED_IN } from "./Query/AuthQuery";
+import { useQuery } from "@apollo/react-hooks";
 
 const App: React.FC = () => {
+  const { data, loading, error } = useQuery(IS_LOGGED_IN);
   return (
     <div className="App">
       <div>Welcome to React App</div>
+      <div>{JSON.stringify(data)}</div>
+      <div>{loading}</div>
+      <div>{error}</div>
     </div>
   );
-}
+};
 
 export default App;
