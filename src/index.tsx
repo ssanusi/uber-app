6b4f07bda8c7dec7d9b66a4779a1018915875be4
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-01-16 20:39:24 +0100

    Setup global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import client from "./client";
 import { ApolloProvider } from "@apollo/react-hooks";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { GlobalStyle } from './global-styles';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </ApolloProvider>,
   document.getElementById("root")
 );
--- src/styled.t.ts
+++ src/styled.t.ts
@@ -4,8 +4,6 @@ import 'styled-components'
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    borderRadius: string
-
     colors: {
       main: string
       secondary: string
